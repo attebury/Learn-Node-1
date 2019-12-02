f73a9de68c63fbde3900cc3d1fbf77a6257dfb1d
John Attebury <attebury@Johns-MacBook-Pro.local>
2019-12-01 19:25:14 -0600

    Finished Lesson 14

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -23,3 +23,24 @@ exports.getStores = async (req, res) => {
     console.log(stores)
     res.render('stores', { title: 'Stores', stores })
 }
+
+exports.editStore = async (req, res) => {
+    // 1. Find the store given the id
+    const store = await Store.findOne({ _id: req.params.id })
+    // 2. Confirm they're the owner of the store
+    // 3. Render out the edit form so the user can update their store
+    res.render('editStore', { title: `Edit ${store.name}`, store})
+
+}
+
+exports.updateStore = async (req, res) => {
+    // 1. Find and update the store
+    // findOneAndUpdate(q, data, options)
+    // new: true returns the new store instead of the old one
+    // 
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true}).exec()
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
+    res.redirect(`/stores/${store._id}/edit`)
+    // 2. Redirect to the store and tell them it worked
+
+}
