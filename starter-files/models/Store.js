62fdfb9320fdf23684c6a07bad116269b0ad6d47
John Attebury <attebury@Johns-MacBook-Pro.local>
2019-12-01 23:35:54 -0600

    Lesson 15

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -4,16 +4,34 @@ const slug = require('slugs')
 
 const storeSchema = new mongoose.Schema({
     name: {
-        type: String
-        , trim: true
-        , required: 'Please enter a store name!'
+        type: String,
+        trim: true,
+        required: 'Please enter a store name!'
+    },
+    slug: String,
+    description: {
+        type: String,
+        trim: true
+    },
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: 'You must supply coordinates!'
+        }],
+        address: {
+            type: String,
+            required: 'You must supply an address!'
+        }
     }
-    , slug: String
-    , description: {
-        type: String
-        , trim: true
-    }
-    , tags: [String]
 })
 
 storeSchema.pre('save', function(next) {
